c15a9cbe3cd791f65de9cbfae8d4004d9908beba
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2015-10-27 13:00:34 +0100

    Filtering by input

--- src/ts/components/products/productsComponent.ts
+++ src/ts/components/products/productsComponent.ts
@@ -4,16 +4,18 @@ import {Component, View, CORE_DIRECTIVES} from 'angular2/angular2';
 import {ProductComponent} from './product/productComponent';
 import {Product} from './product/product';
 import {SortComponent} from '../common/sortComponent';
+import {FilterComponent} from '../common/filterComponent';
 
 @Component({
     selector: 'products'
 })
 @View({
     templateUrl: 'src/template/products.html',
-    directives: [ProductComponent, SortComponent, CORE_DIRECTIVES]
+    directives: [ProductComponent, SortComponent, FilterComponent, CORE_DIRECTIVES]
 })
 export class ProductsComponent {
     public products:Product[];
+    public filteredProducts:Product[];
     public product:Product;
     public sorting;
 
@@ -27,6 +29,7 @@ export class ProductsComponent {
             new Product('2nd name', 'longer description', 'http://lorempixel.com/400/200?2', 250, ['wow', 'such good']),
             new Product('acs', 'desc', 'http://lorempixel.com/400/200?3', 120, ['test'])
         ];
+        this.filteredProducts = this.products;
     }
 
     sort($event) {
@@ -38,6 +41,12 @@ export class ProductsComponent {
                 return this.sorting.desc ? 1 : -1;
             else
                 return this.sorting.desc ? -1 : 1;
+        });
+    }
+
+    filter($event) {
+        this.filteredProducts = this.products.filter(function(product:Product) {
+            return product.name.startsWith($event);
         });
     }
 }
